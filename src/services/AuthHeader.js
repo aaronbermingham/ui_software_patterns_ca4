970f6a2906d7351276e8cfc0b42c01685f5e2fd6
Aaron Bermingham <aaronbrmnghm@gmail.com>
2021-04-10 10:38:33 +0100

    User can register and login

---
+++ src/services/AuthHeader.js
@@ -0,0 +1,10 @@
+export default function AuthHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log("Access token auth header method ", user.accessToken)
+    if (user && user.accessToken) {
+      return { Authorization: 'Bearer ' + user.accessToken }; 
+    } else {
+      return {};
+    }
+  }
+  
